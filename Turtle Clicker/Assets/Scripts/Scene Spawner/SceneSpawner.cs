8a465290dbfc1682b2679f6ecceb9553bb74841d
TurtleGameDev <theturtleofawesomeness@gmail.com>
2017-12-23 07:48:20 -0800

    Added turtle button and text box to display numbers

--- Turtle Clicker/Assets/Scripts/Scene Spawner/SceneSpawner.cs
+++ Turtle Clicker/Assets/Scripts/Scene Spawner/SceneSpawner.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class SceneSpawner : MonoBehaviour
 {
-	void Start ()
+    
+    public void Start()
     {
-		
-	}
+        GameObject Camera = GameObject.Instantiate((GameObject)Resources.Load("Camera/Camera"));
+        Camera.name = "Camera";
+        GameObject count = GameObject.Instantiate((GameObject)Resources.Load("Counts/Count"));
+        count.name = "Count";
+        GameObject uidisplay = GameObject.Instantiate((GameObject)Resources.Load("UI/Canvas"));
+        uidisplay.name = "Canvas";
+        GameObject Event = GameObject.Instantiate((GameObject)Resources.Load("UI/EventSystem"));
+        Event.name = "EventSystem";
+        GameObject.Destroy(this.gameObject);
+    }
 }
